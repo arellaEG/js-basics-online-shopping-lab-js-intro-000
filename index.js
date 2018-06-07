3cd31b6ea3dd5e56f1d973c4c00cbd3dfb74a815
Arella Gussow <arellaeg@gmail.com>
2018-06-07 06:47:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (typeof cardNumber==='undefined'){
-    return `Sorry, we don't have a credit card number on file for you.`
+    return `Sorry, we don't have a credit card on file for you.`
   }
   else {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
