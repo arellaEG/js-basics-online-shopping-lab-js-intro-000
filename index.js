f08c561640b4415b5cb862de190449d77f6b015a
Arella Gussow <arellaeg@gmail.com>
2018-06-07 06:42:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card number on file for you.`
   }
   else {
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
