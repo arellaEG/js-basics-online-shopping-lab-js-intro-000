f33312360ecaa852654273092557c7daa6c7d097
Arella Gussow <arellaeg@gmail.com>
2018-06-07 06:37:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,7 +42,9 @@ function total() {
 function removeFromCart(item) {
   for (let i=0; i<cart.length; i++){
     if cart[i]['itemName']===item{
+      var rem=i
       cart.splice(i,1)
+      return cart
     }
   }
 }
