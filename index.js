1b050298b933afed34dd4f632e9fec9c9edd26ce
Arella Gussow <arellaeg@gmail.com>
2018-06-07 06:43:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber===''){
+  if (cardNumber.length<1){
     return `Sorry, we don't have a credit card number on file for you.`
   }
   else {
