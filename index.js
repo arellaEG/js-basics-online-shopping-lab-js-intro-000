eeaa723387b64d3d76f2cafa39208ed786918946
Arella Gussow <arellaeg@gmail.com>
2018-06-07 06:18:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function viewCart() {
   }
   else {
     var str=`In your cart, you have`;
-    for (let i=0; i<cart.length-2; i++){
+    for (var i=0; i<cart.length-2; i++){
       str=str + `${cart.i['itemName']} at ${cart.i['itemPrice']}, `}
     str=str+`and ${cart.i['itemName']} at ${cart.i['itemPrice']}.`
     return str
